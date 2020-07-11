596b0ffecbf826e6da5cce57fdf37f7c96e37439
Joep Dumont <joep@dumont.nl>
2020-07-11 16:05:03 +0200

    set config to test for jumping to new banks and circularity

--- vitis/Circular_Buffer/src/config.h
+++ vitis/Circular_Buffer/src/config.h
@@ -6,12 +6,12 @@
 #define TDATA_WIDTH_BIT 256 // bits [set in vivado tfcg IP]
 #define TDATA_WIDTH_BYTE (TDATA_WIDTH_BIT / 8) // bits [set in vivado tfcg IP]
 
-#define PKT_SIZE_KBYTE 1 // In KB
+#define PKT_SIZE_KBYTE 15 // In KB
 #define PKT_SIZE_BYTE PKT_SIZE_KBYTE * 1024 // in B
 
 #define PKT_LEN (PKT_SIZE_BYTE / TDATA_WIDTH_BYTE) // data elements per packet
 
-#define PKT_CNT 10 // how many packets tfcg sends before stop
+#define PKT_CNT 16 // how many packets tfcg sends before stop
 
 enum DMA_AMOUNT {
 	N_EXT_DMA = 1,
